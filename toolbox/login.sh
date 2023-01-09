77a52e22c9aa27978fe3631cbe58f1366a5042dc
Shakir <sralmasaari@gmail.com>
2023-01-08 20:17:58 -0800

    Add test playbook and modify login rc

--- toolbox/login.sh
+++ toolbox/login.sh
@@ -1,4 +1,69 @@
 
+# Default bashrc***********************************************************************
+case $- in
+    *i*) ;;
+      *) return;;
+esac
+
+HISTCONTROL=ignoreboth
+
+shopt -s histappend
+
+HISTSIZE=1000
+HISTFILESIZE=2000
+
+shopt -s checkwinsize
+
+if [ -z "${debian_chroot:-}" ] && [ -r /etc/debian_chroot ]; then
+    debian_chroot=$(cat /etc/debian_chroot)
+fi
+
+# set a fancy prompt (non-color, unless we know we "want" color)
+case "$TERM" in
+    xterm-color|*-256color) color_prompt=yes;;
+esac
+
+if [ -n "$force_color_prompt" ]; then
+    if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
+	color_prompt=yes
+    else
+	color_prompt=
+    fi
+fi
+
+if [ "$color_prompt" = yes ]; then
+    PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+else
+    PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\$ '
+fi
+unset color_prompt force_color_prompt
+
+case "$TERM" in
+xterm*|rxvt*)
+    PS1="\[\e]0;${debian_chroot:+($debian_chroot)}\u@\h: \w\a\]$PS1"
+    ;;
+*)
+    ;;
+esac
+
+if [ -x /usr/bin/dircolors ]; then
+    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+    alias ls='ls --color=auto'
+fi
+
+if [ -f ~/.bash_aliases ]; then
+    . ~/.bash_aliases
+fi
+
+if ! shopt -oq posix; then
+  if [ -f /usr/share/bash-completion/bash_completion ]; then
+    . /usr/share/bash-completion/bash_completion
+  elif [ -f /etc/bash_completion ]; then
+    . /etc/bash_completion
+  fi
+fi
+# End of default bashrc****************************************************
+
 # *************************************************************************
 #
 # Script to print useful system information on login.
@@ -63,17 +128,15 @@ function resourcesInfo() {
 }
 
 function LoggedInUsers () {
-    echo -e -n "$Dim[Users-In]"
-    # PROCESSES=$(ps aux | wc -l)
-    # echo -e -n $ColorReset"➜ [$Bold$Blue Running Processes$ColorReset$Normal $Yellow ◉ $PROCESSES $ColorReset] "
     # Get logged in users from tty entry:
+    echo -e -n "$Dim[Users-In]"
     # (NR>2) tells awk to skip two header lines
     # sort  and  uniq  to remove duplicate users
     # last sed is to trim/prevent printing whitespaces
     TTY_USERS=$(w | awk '(NR>2) { print $1 }' | sort | uniq | sed '/^[[:space:]]*$/d')
     
-    # I'm using echo to utilize -n and -e to force printing mutiline stdout in a single line 
-    # because if there're 2+ users logged in, there'd be 2 IP addresses sent out to stdout from the 'w' command
+    # Using echo here to print mutiline stdout in a single line via flags: -n and -e
+    # To handle a multi-user log in situation. More than one IP will be sent out to stdout from the 'w' command.
     # ->> awk explanation: skip two headers, print third column, sort, get unique IPs, trim space lines
     TTY_IP=$(echo -n -e "$(w | awk '(NR>2) { print $3 }' | sort | uniq | sed '/^[[:space:]]*$/d')")
     
@@ -82,13 +145,13 @@ function LoggedInUsers () {
 
 function serviceStatus () {
     IS_ACTIVE=$(systemctl is-active "$1")
-    IS_RUNNING=$(systemctl show -p SubState --value "$1")
+    IS_UP=$(systemctl show -p SubState --value "$1")
     if [ "$IS_ACTIVE" = "active" ] ; then
     echo -e -n "$ColorReset""➜ [$Bold$Blue $1$ColorReset$Normal $Green ✓$ColorReset$Dim active "
 
-        case $IS_RUNNING in
-            "running") echo -e -n "$Green▲$ColorReset$Dim running$Normal$ColorReset ]" ;;
-            "dead") echo -e -n "$Red▼$ColorReset$Dim dead$ColorReset ]" ;;
+        case $IS_UP in
+            "up") echo -e -n "$Green▲$ColorReset$Dim up$Normal$ColorReset ]" ;;
+            "down") echo -e -n "$Red▼$ColorReset$Dim down$ColorReset ]" ;;
             *) echo -e -n "Error loading service" ;;
         esac
 
